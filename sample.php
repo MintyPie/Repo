17c8324adc6c338f83dd847ab8f01edb41278372
MintyPie <prusskaya@gmail.com>
2015-10-06 21:26:57 +0300

    my first files

---
+++ sample.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Тестовая страничка</title>
+</head>
+<body>
+Привет, HTML!
+<br/>
+<?php echo (new \DateTime())->format('c');?>
+</body>
+</html>
